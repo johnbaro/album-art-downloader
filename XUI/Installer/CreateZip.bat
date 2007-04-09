83d7efaf27ec8808ea0fade88dd0c3ea3d02c2dd
alexvallat <alexvallat@8d1bc4b6-e811-4374-9065-c5b6b76b2f37>
2007-04-09 16:19:35 +0000

    Added CreateZip.bat to create a zip file for release. Requires 7-zip to be installed.

---
+++ XUI/Installer/CreateZip.bat
@@ -0,0 +1,8 @@
+@echo off
+set version=0.3
+
+set zipfile=..\..\Releases\AlbumArtDownloaderXUI-%version%.zip
+del %zipfile%
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %zipfile% @ziplist.txt -x!*\.svn
+set version=
+set zipfile=
