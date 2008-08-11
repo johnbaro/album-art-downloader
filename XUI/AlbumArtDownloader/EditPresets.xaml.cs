2a3f0c5c851710eb565aa8dac5165b90eee3b83a
alexvallat <alexvallat@8d1bc4b6-e811-4374-9065-c5b6b76b2f37>
2008-08-11 18:58:22 +0000

    Presets Editor WIP

---
+++ XUI/AlbumArtDownloader/EditPresets.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using AlbumArtDownloader.Controls;
+
+namespace AlbumArtDownloader
+{
+	/// <summary>
+	/// Interaction logic for EditPresets.xaml
+	/// </summary>
+	public partial class EditPresets : Window
+	{
+		private ObservableCollection<Preset> mPresets = new ObservableCollection<Preset>();
+
+		public EditPresets()
+		{
+			mPresets.Add(new Preset("hello", "there"));
+			mPresets.Add(new Preset("another", "line"));
+			InitializeComponent();
+
+			CommandBindings.Add(new CommandBinding(EditableCell.Commands.Edit, EditExec, EditCanExec));
+		}
+
+		private void EditExec(object sender, ExecutedRoutedEventArgs e)
+		{
+			//IsEditing = true;
+		}
+
+		private void EditCanExec(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = mPresetsList.SelectedItems.Count == 1;
+		}
+		
+		public ObservableCollection<Preset> Presets
+		{
+			get { return mPresets; }
+		}
+
+	}
+}
---
+++ XUI/AlbumArtDownloader/Preset.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace AlbumArtDownloader
+{
+	public class Preset : DependencyObject
+	{
+		public Preset()
+		{
+		}
+
+		public Preset(string name, string value)
+		{
+			Name = name;
+			Value = value;
+		}
+
+		#region Name
+		public static readonly DependencyProperty NameProperty = FrameworkElement.NameProperty.AddOwner(typeof(Preset));
+		public string Name
+		{
+			get { return (string)GetValue(NameProperty); }
+			set { SetValue(NameProperty, value); }
+		}
+		#endregion
+
+		#region Value
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(Preset));
+
+		public string Value
+		{
+			get { return (string)GetValue(ValueProperty); }
+			set { SetValue(ValueProperty, value); }
+		}
+		#endregion
+	}
+}
